fd67a017d591542b4e45027ceb0dbce71a59ac84
joaquin telleria <joaquintelleria@gmail.com>
2021-11-11 01:26:37 +0100

    Create cropimages.m

---
+++ functions/cropimages.m
@@ -0,0 +1,10 @@
+       function cropImages(image,P1, P2, P3, P4) % crops all images
+             selectedDirectory = uigetdir(); % get directory from user, we will store all the cropped images there
+
+            if (selectedDirectory == 0) % Make sure user didn't cancel uigetdir dialog
+                return;
+            end
+            
+                image = imcrop(image,[P1 P2 P3 P4]); % crop the image within the cropping box
+                imwrite(image,strcat(selectedDirectory,"\_cropped.png")); % save the image on the chosen directory
+       end
