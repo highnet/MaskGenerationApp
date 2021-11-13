ee1bd036326590152ecb7f2be29e46f79474afc2
joaquin telleria <joaquintelleria@gmail.com>
2021-11-13 17:06:44 +0100

    Delete manualthresholding.m

--- functions/manualthresholding.m
+++
@@ -1,9 +0,0 @@
-        function results = manualThresholding(image,threshold,flipped)
-            
-            maskedImage = rgb2gray(image) >= threshold; % compute a binary mask (>=)
-            
-            if flipped == 1
-                maskedImage = ~maskedImage;
-            end
-            results = maskedImage;
-        end
