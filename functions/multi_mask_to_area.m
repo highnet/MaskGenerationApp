8ec515c0939c1697b5eed75764abaa4a0cc17881
joaquin telleria <joaquintelleria@gmail.com>
2021-11-13 17:56:28 +0100

    Create multi_mask_to_area.m

---
+++ functions/multi_mask_to_area.m
@@ -0,0 +1,14 @@
+function result = multi_mask_to_area(masks,spatial_resolution_meters)
+
+numberOfMasks = size(masks);
+numberOfMasks = numberOfMasks(2);
+
+areas = cell(1,numberOfMasks);
+
+for i = 1:numberOfMasks
+    areas{1,i} = sum(masks{1,i}(:)) * spatial_resolution_meters;
+end
+
+result = areas;
+
+end
