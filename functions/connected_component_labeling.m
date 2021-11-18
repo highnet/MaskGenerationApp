f84d65e1cf660b7faa612c5c8744dcf467fee56d
svetkavia1 <44173698+svetkavia1@users.noreply.github.com>
2021-11-18 13:38:48 +0100

    Added functions for labeling connected components

---
+++ functions/connected_component_labeling.m
@@ -0,0 +1,11 @@
+function result = connected_component_labeling(image, conn)
+
+    grayImage = rgb2gray(image);
+
+    switch conn
+        case 4
+            result = bwlabel(grayImage, 4);
+        otherwise
+            result = bwlabel(grayImage, 8);
+    end
+end
---
+++ functions/multi_connected_component_labeling.m
@@ -0,0 +1,21 @@
+function result = multi_connected_component_labeling(images, conn)
+
+    numberOfImages = size(images);
+    numberOfImages = numberOfImages(2);
+
+    masks = cell(1,numberOfImages);
+
+    for i = 1:numberOfImages
+        grayImage = rgb2gray(images{1,i})
+        
+        switch conn
+            case 4
+                maskedImage = bwlabel(grayImage, 4);
+            otherwise
+                maskedImage = bwlabel(grayImage, 8);
+        end
+        masks(1, i) = maskedImage;
+        
+    end
+    result = masks;
+end
