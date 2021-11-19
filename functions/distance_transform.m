575d65854716c544dd39dec26180f647724b99a9
e11727255 <e11727255@student.tuwien.ac.at>
2021-11-19 21:57:20 +0100

    Testing distance transform

---
+++ functions/distance_transform.m
@@ -0,0 +1,29 @@
+function [thickness] = distance_transform(img)
+% Find all unique river channels and assign them an ID
+[label,N] = bwlabel(img);
+
+% Initialize an array that determines the width
+thickness = zeros(N,1);
+
+% For each "channel"
+for index = 1 : N
+
+    countour = label == index; % Get a mask that segments out the parts of the river flow (channels)
+    d = bwdist(~countour); % apply distance transform on the inverse of that img
+
+
+    maxDistance = max(d(:)); % Get the maximum distance to the edge
+    distances = d(abs(d - maxDistance) <= 1); % Collect those distances within a tolerance, in this case lower than 1
+    thickness(index) = 2*mean(distances);  % Find the average width and multiply by 2 for full width
+
+
+    % the following bit is me testing the code, it should show thw width at
+    % each point, cant figure it out quite good enough for now
+% imshow(im); hold on;
+% for index = 1 : N
+%     [y,x]= find(label == index);
+%     center = mean([x y]);
+%     text(center(1), center(2), ['T = ' num2str(thickness(index))], 'color', 'red');
+% end
+end
+
