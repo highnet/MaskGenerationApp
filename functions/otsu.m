15255bf017c284f0ba49771d6b9e6832a8e0a512
joaquin telleria <joaquintelleria@gmail.com>
2021-11-20 01:35:14 +0100

    Update otsu.m

--- functions/otsu.m
+++ functions/otsu.m
@@ -1,38 +1,37 @@
-% Returns
+% Given an image, return a mask of the image generated with Otsu's method.
 % inputs:
-%
-%
+% image: The image to be masked.
+% flipped: Whether the mask should be flipped or not.
 
 % outputs:
-%
-%
+% result: The masked image.
 
 function result = otsu(image, flipped)
 
-grayImage = rgb2gray(image);
-top = 256;
-maximumInterClassVariance = 0.0;
+grayImage = rgb2gray(image); % Convert the image to single channel grayscale
+top = 256; % top defines the maximum possible value of the grayscale image, in our case for uint8 images that value is 256
+maximumInterClassVariance = 0.0; % this variable stores the maximum inter class variance for Otsu's method
 
-for i = 1:top
-    foreGroundPixels = grayImage >= i;
-    backGroundPixels = ~foreGroundPixels;
+for i = 1:top % iterate through every possible mask
+    foreGroundPixels = grayImage >= i; % generate the foreground pixels
+    backGroundPixels = ~foreGroundPixels; % generate the background pixels
 
-    numberOfPixelsInForeground = sum(foreGroundPixels(:) == 1);
-    meanIntensityOfForeground = mean(grayImage(foreGroundPixels));
+    numberOfPixelsInForeground = sum(foreGroundPixels(:) == 1); % count the foreground pixels
+    meanIntensityOfForeground = mean(grayImage(foreGroundPixels)); % calculate the average value in the original image of the foreground pixels
 
-    numberOfPixelsInBackground = sum(backGroundPixels(:) == 1);
-    meanIntensityOfBackground = mean(grayImage(backGroundPixels));
+    numberOfPixelsInBackground = sum(backGroundPixels(:) == 1); % count the background pixels
+    meanIntensityOfBackground = mean(grayImage(backGroundPixels)); % calculate the average value in the original image of the background pixels
 
-    interClassVariance = numberOfPixelsInBackground * numberOfPixelsInForeground * (meanIntensityOfBackground - meanIntensityOfForeground)^2;
+    interClassVariance = numberOfPixelsInBackground * numberOfPixelsInForeground * (meanIntensityOfBackground - meanIntensityOfForeground)^2; % calculate the inter class variance for this iteration
 
-    if (interClassVariance >= maximumInterClassVariance)
-        level = i;
-        maximumInterClassVariance = interClassVariance;
+    if (interClassVariance >= maximumInterClassVariance) % is this the largest inter class variance we have ever seen?
+        level = i; % if so, store the level...
+        maximumInterClassVariance = interClassVariance; % ...and store the new record
     end
 end
-maskedImage = grayImage <= level;
-if flipped == 1
-    maskedImage = ~maskedImage;
+maskedImage = grayImage <= level; % Once we have our optimal level, generate the final mask with that level
+if flipped == 1 % does the user want to flip the mask?
+    maskedImage = ~maskedImage; % if so, flip the mask
 end
-result = maskedImage;
+result = maskedImage; % return the masked image
 end
