9ee26942eb0432913fca4ed2fe716535dd040978
joaquin telleria <joaquintelleria@gmail.com>
2021-11-20 01:40:07 +0100

    Update multi_mask_to_area.m

--- functions/multi_mask_to_area.m
+++ functions/multi_mask_to_area.m
@@ -1,11 +1,12 @@
-% Returns 
-% inputs: 
-%  
-% 
+% Author: Joaquin Telleria (01408189)
+% Given a cell of masks, returns a cell of areas (in squared meters) of foreground pixels (==1) of each image.
+% inputs:
+% masks: A [1xN] cell of logical matrices, whereby in each matrix water pixels are foreground pixels (==1).
+% spatial_resolution_meters: The spatial resolution of the satellite image,  1 pixel = spatial_resolution_meters squared meters.
 
-% outputs: 
-%
-%
+% outputs: masks
+% result: A [1xN] cell of areas, each cell contains the area of foreground pixels of each input image.
+
 
 function result = multi_mask_to_area(masks,spatial_resolution_meters)
 
