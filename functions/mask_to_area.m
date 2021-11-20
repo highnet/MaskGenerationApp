e1526f6a9b1f58f7397b22dd7864952cc4e52b68
joaquin telleria <joaquintelleria@gmail.com>
2021-11-20 01:22:51 +0100

    Update mask_to_area.m

--- functions/mask_to_area.m
+++ functions/mask_to_area.m
@@ -1,16 +1,15 @@
-% Returns 
-% inputs: 
-%  
-% 
+% Given a mask, returns the area (in squared meters) of foreground pixels (==1).
+% inputs:
+% mask: A logical matrix, whereby water pixels are foreground pixels (==1).
+% spatial_resolution_meters: The spatial resolution of the satellite image,  1 pixel = spatial_resolution_meters squared meters.
 
-% outputs: 
-%
-%
+% outputs:
+% result: The area of foreground pixels.
 
 function result = mask_to_area(mask,spatial_resolution_meters)
 
-area = sum(mask(:)) * spatial_resolution_meters;
+area = sum(mask(:)) * spatial_resolution_meters; % Multiply the count of foreground pixels (==1) with the spatial resolution in meters.
 
-result = area;
+result = area; % return the result.
 
 end
