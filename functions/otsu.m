2c31e83fdfbf9900bee5164e766ab1c5f26a4585
joaquin telleria <joaquintelleria@gmail.com>
2021-11-20 01:16:35 +0100

    Update otsu.m

--- functions/otsu.m
+++ functions/otsu.m
@@ -1,38 +1,38 @@
-% Returns 
-% inputs: 
-%  
-% 
-
-% outputs: 
+% Returns
+% inputs:
 %
 %
 
-        function result = otsu(image, flipped)
-            
-            grayImage = rgb2gray(image); 
-            top = 256;
-            maximumInterClassVariance = 0.0;
-            
-            for i = 1:top
-                foreGroundPixels = grayImage >= i;
-                backGroundPixels = ~foreGroundPixels;
-                
-                numberOfPixelsInForeground = sum(foreGroundPixels(:) == 1);
-                meanIntensityOfForeground = mean(grayImage(foreGroundPixels));
-                
-                numberOfPixelsInBackground = sum(backGroundPixels(:) == 1);
-                meanIntensityOfBackground = mean(grayImage(backGroundPixels));
-                
-                interClassVariance = numberOfPixelsInBackground * numberOfPixelsInForeground * (meanIntensityOfBackground - meanIntensityOfForeground)^2;
-                
-                if (interClassVariance >= maximumInterClassVariance)
-                    level = i;
-                    maximumInterClassVariance = interClassVariance;
-                end
-            end
-              maskedImage = grayImage <= level;
-            if flipped == 1
-                maskedImage = ~maskedImage;
-            end
-              result = maskedImage;
-        end
+% outputs:
+%
+%
+
+function result = otsu(image, flipped)
+
+grayImage = rgb2gray(image);
+top = 256;
+maximumInterClassVariance = 0.0;
+
+for i = 1:top
+    foreGroundPixels = grayImage >= i;
+    backGroundPixels = ~foreGroundPixels;
+
+    numberOfPixelsInForeground = sum(foreGroundPixels(:) == 1);
+    meanIntensityOfForeground = mean(grayImage(foreGroundPixels));
+
+    numberOfPixelsInBackground = sum(backGroundPixels(:) == 1);
+    meanIntensityOfBackground = mean(grayImage(backGroundPixels));
+
+    interClassVariance = numberOfPixelsInBackground * numberOfPixelsInForeground * (meanIntensityOfBackground - meanIntensityOfForeground)^2;
+
+    if (interClassVariance >= maximumInterClassVariance)
+        level = i;
+        maximumInterClassVariance = interClassVariance;
+    end
+end
+maskedImage = grayImage <= level;
+if flipped == 1
+    maskedImage = ~maskedImage;
+end
+result = maskedImage;
+end
