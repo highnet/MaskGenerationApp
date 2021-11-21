cc57dc973b00324a109a8be35276e722be26dd39
e11727255 <e11727255@student.tuwien.ac.at>
2021-11-21 11:24:06 +0100

    Output changed, documentation added

--- functions/distance_transform.m
+++ functions/distance_transform.m
@@ -1,31 +1,43 @@
-function [thickness] = distance_transform(img)
-% Find all unique river channels and assign them an ID
-Img=img;
-bwImg=im2bw(Img, 0.45);
-[label,N] = bwlabel(bwImg);
+function [maxDistances, dAtCords] = distance_transform(img,CCM, coordinates)
+%img is a pre-processsed image (i.e. with dilation, erosion and region growig)
+%CCM represents a mask with conected components
+%coordinates is an optional input used for calls in GUI
 
-% Initialize an array that determines the width
-thickness = zeros(N,1);
-
-% For each "channel"
-for index = 1 : N
-
-    countour = label == index; % Get a mask that segments out the parts of the river flow (channels)
-    d = bwdist(~countour); % apply distance transform on the inverse of that img
-
-
-    maxDistance = max(d(:)); % Get the maximum distance to the edge
-    distances = d(abs(d - maxDistance) <= 1); % Collect those distances within a tolerance, in this case lower than 1
-    thickness(index) = 2*mean(distances);  % Find the average width and multiply by 2 for full width
-
-
-    % the following bit is me testing the code, it should show thw width at
-    % each point, cant figure it out quite good enough for now
-% imshow(bwImg); hold on;
-% for index = 1 : N
-%     [y,x]= find(label == index);
-%     center = mean([x y]);
-%     text(center(1), center(2), ['T = ' num2str(thickness(index))], 'color', 'red');
-% end
+if ~exist('coordinates','var')
+     %if the third parameter does not exist, we default it to something
+      coordinates = [1 1];
 end
+
+%if the passed img is not a sigle channel grayscale, convert it to such
+grayImg=im2gray(img);
+
+%convert the image to a binary image using Otsu's threshold
+%get the threshold
+level=graythresh(grayImg);
+%use the threshold for creating a binary img
+bwImg=imbinarize(grayImg, level);
+
+%get the number of labels for conected components
+[label,N] = bwlabel(CCM); 
+%N=number of components
+%label=mask with positive integers recognising diferent components
+imshow(label)
+
+%array with max distances for each component
+maxDistances=[];
+
+% For each component
+for index = 1 : N
+    %a mask representing an individual component
+    component = label == index;  
+    d = bwdist(component); % apply distance transform 
+    maxDistance = max(d(:)); % Get the maximum distance to the edge
+    maxDistances=cat(1,maxDistances, maxDistance);
+end
+    
+temp=bwdist(bwImg);
+dAtCords=temp(coordinates(1),coordinates(2));
+end
+
+
 
