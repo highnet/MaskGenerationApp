74909cdbc323a43af4250ccede29a6d66f04d9fa
Maximilian Seifried <80042049+Maximilian-Seifried@users.noreply.github.com>
2021-11-22 12:43:06 +0100

    Added documentation

--- functions/multi_morph_operation.m
+++ functions/multi_morph_operation.m
@@ -1,20 +1,21 @@
-% Returns 
+% Author: Maximilian Jack Seifried (11809634)
+% Returns a mask for each of the given images with the morphological operation specified by type applied.
 % inputs: 
-%  
-% 
+%  masks: A list of images to be masked.
+%  type: A string that is one of erode, dilate, open or close, that specifies the operation type.
+%  SE: A structural element with which the morph. operation is applied.
 
 % outputs: 
-%
-%
+%  result: A list of the masked images.
 
 function result = multi_morph_operation(masks, type, SE)
 
-    numberOfImages = size(masks);
-    numberOfImages = numberOfImages(2);
+    numberOfImages = size(masks); % store the number of images to loop through
+    numberOfImages = numberOfImages(2); % we only need the number of columns
 
-    morphs = cell(1,numberOfImages);
+    morphs = cell(1,numberOfImages); % the list that stores the morphed images
 
-    for i = 1:numberOfImages
+    for i = 1:numberOfImages % iterate through every input image
         mask = masks{1,i};
 
         switch type
